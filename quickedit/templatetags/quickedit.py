cf2494d3a94e8608fce6e02438b64cb651fc4410
Justin Quick <justquick@gmail.com>
2010-08-27 11:36:56 -0400

    fixed quickedit to work with non formset forms

--- quickedit/templatetags/quickedit.py
+++ quickedit/templatetags/quickedit.py
@@ -5,7 +5,7 @@ from django.forms.models import modelform_factory
 
 register = Library()
 
-def results(cl):
+def results(cl,r):
     qe = hasattr(cl.model_admin, 'quick_editable')
     if cl.formset:
         if qe:
@@ -13,7 +13,7 @@ def results(cl):
                 yield {
                     'fields':list(items_for_result(cl, res, form)), 
                     'quickedit':form
-                }#'quickedit':form(instance=res)}
+                }
         else:
             for res, form in zip(cl.result_list, cl.formset.forms):
                 yield list(items_for_result(cl, res, form))
@@ -22,19 +22,17 @@ def results(cl):
             for res in cl.result_list:
                 yield {
                     'fields':list(items_for_result(cl, res, None)), 
-                    'quickedit':form
-                }#'quickedit':form(instance=res)}
+                    'quickedit':cl.model_admin.get_changelist_formset(r).form(instance=res)
+                }
         else:
             for res in cl.result_list:
                 yield list(items_for_result(cl, res, None))
 
 def qe_result_list(context, cl):
-    static_url = context.has_key('STATIC_URL') and 'STATIC_URL' or 'MEDIA_URL'
     return {
         'cl': cl,
         'result_headers': list(result_headers(cl)),
-        'results': list(results(cl)),
-        'STATIC_URL': context[static_url]
+        'results': list(results(cl,context['request'])),
     }
 qe_result_list = register.inclusion_tag('quickedit/change_list_results.html',
                         takes_context=True)(qe_result_list)
